f0dd3863098f1412e7f0359becbec9ff951114e8
Antonin <antonin.joly.monrigal@gmail.com>
2022-01-23 10:28:54 +0900

    ElasticSearch & Kibana set. RetinaNet datagenerator under test.
    Security authentication for ES and kibana to be tested.

--- src/spine/dataset/process_dataset.py
+++ src/spine/dataset/process_dataset.py
@@ -8,11 +8,12 @@ from PIL import Image
 import io
 from tqdm import tqdm
 
-def process_json_to_img(base_dir, seed):
-
-    json_path = [i for i in os.listdir(os.path.join(base_dir)) if os.path.splitext(os.path.basename(i))[1]=='.json'][seed]
+def process_json_to_img(base_dir, seed=0, path=None):
 
     try:
+        json_list = [i for i in os.listdir(os.path.join(base_dir)) if os.path.splitext(os.path.basename(i))[1]=='.json']
+        json_path = json_list[seed] if path==None else path
+
         with open(os.path.join(base_dir, json_path)) as f:
             json_pic = json.load(f)
         img_data = json_pic['imageData']
@@ -23,14 +24,18 @@ def process_json_to_img(base_dir, seed):
         np.random.seed(0)
         num_col = len(json_pic['shapes'])
         colors = [(np.random.choice(range(256), size=3)).tolist() for i in range(num_col)]
-
+        vis = img.copy()
         for idx, i in enumerate(json_pic['shapes']):
             pts = np.array(i['points'])
-            img = cv2.polylines(img,np.int32([pts]), 1, colors[idx], 15)
-        return img
+            vis = cv2.polylines(vis, np.int32([pts]), 1, colors[idx], 15)
+
+        vrt = [i['points'] for i in json_pic['shapes']]
+        vrt_shape = [len(i) for i in vrt]
+        vrt = np.vstack(vrt).astype(np.int0)
+        
+        return vis, img, vrt, vrt_shape
 
     except Exception as e:
-        print(name)
         print(e)
         return None
         
